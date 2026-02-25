1e07295a4c1b7d168c255f1d18c1f46b7901d2fe
marceoroq <oroquieta.m@gmail.com>
2026-02-24 21:43:02 -0300

    feat: add environment variable for contact email

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -10,10 +10,18 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [
     preact({
       compat: true,
     }),
   ],
+  env: {
+    schema: {
+      CONTACT_EMAIL: envField.string({
+        context: "server",
+        access: "public",
+        default: "testing@testing.com",
+      }),
+    },
+  },
 });
